906eaf32bf9ae3132429686f9d579c0e6984774a
Stefan Aleksic <stefan@stefanaleksic.com>
2017-09-25 22:06:01 -0700

    Create simple typist component.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
 
-const buttonStyles = {
-  border: '1px solid #eee',
-  borderRadius: 3,
-  backgroundColor: '#FFFFFF',
-  cursor: 'pointer',
-  fontSize: 15,
-  padding: '3px 10px',
-  margin: 10
-};
-
-const Button = ({ children, onClick }) => (
-  <button
-    style={buttonStyles}
-    onClick={onClick}
-  >
-    {children}
-  </button>
+const Typist = ({ text }) => (
+  <span> {text} </span>
 );
 
-Button.propTypes = {
-  children: React.PropTypes.string.isRequired,
-  onClick: React.PropTypes.func
+Typist.propTypes = {
+  text: React.PropTypes.string.isRequired
 };
 
-export default Button;
+export default Typist;
