a9a9c630a0a917cc8231241fc9696a993feb924e
Louis Vichy <lgvichy@gmail.com>
2017-09-26 03:58:20 -0400

    Trimming the code, adding children for props, adding storybook with children

--- src/index.js
+++ src/index.js
@@ -1,53 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Typographer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { statefulGenerator: this.generator(props.text) };
-    }
+export default class Typographer extends React.PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			statefulGenerator: this.generator(props.children || props.text)
+		};
+	}
 
-    componentDidMount() {
-        this.typingTimer = setInterval(
-            () => this.tick(), this.props.speed
-        );
-    }
+	componentDidMount() {
+		this.typingTimer = setInterval(() => this.tick(), this.props.speed);
+	}
 
-    componentWillUnmount() {
-        clearInterval(this.typingTimer);
-    }
+	componentWillUnmount() {
+		clearInterval(this.typingTimer);
+	}
 
-    render() {
-        var statefulGenerator = this.state.statefulGenerator;
-        return (<span> {statefulGenerator.text} </span>);
-    }
+	tick() {
+    const {statefulGenerator} = this.state;
+		this.setState({
+			statefulGenerator: statefulGenerator.next()
+		});
+	}
 
-    tick() {
-        this.setState({
-            statefulGenerator: this.state.statefulGenerator.next()
-        });
-    }
+	generator(text) {
+		return this.generatorHelper(text, 0);
+	}
 
-    generator(text) {
-        return this.generatorHelper(text, 0);
-    }
+	generatorHelper(text, currentIndex) {
+		const toReturn = {
+			text: text.substring(0, currentIndex + 1),
+			next: () => this.generatorHelper(text, currentIndex + 1),
+		};
 
-    generatorHelper(text, currentIndex) {
-        const toReturn = {
-            text: text.substring(0, currentIndex + 1),
-            next: () => this.generatorHelper(text, currentIndex + 1)
-        };
-        if (currentIndex >= text.length) {
-            return {
-                ...toReturn,
-                finished: true
-            };
-        } else {
-            return  {
-                ...toReturn
-            };
-        };
-    }
+		return {
+			...toReturn,
+			finished: currentIndex < text.length,
+		};
+	}
+
+  render() {
+    const {statefulGenerator} = this.state;
+    return (
+      <span>
+        {statefulGenerator.text}
+      </span>
+    );
+  }
+
 }
-
-export default Typographer;
--- stories/index.js
+++ stories/index.js
@@ -5,4 +5,9 @@ import Typographer from './../src';
 storiesOf('Typographer', module)
   .add('with text', () => (
     <Typographer text="Hello Button" speed = {100}/>
-  ));
+  ))
+  .add('with children', () => (
+    <Typographer speed = {100}>
+      Hello Button
+    </Typographer>
+  ));;
